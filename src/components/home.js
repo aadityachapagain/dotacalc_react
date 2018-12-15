65910530e33fd62225aa24345054e947916431e2
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-15 15:02:59 +0545

    design the home

--- src/components/home.js
+++ src/components/home.js
@@ -7,11 +7,14 @@ import heros from '../resources/heros_blur.jpg'
 import counter from '../resources/counters_blur.jpg'
 import predict from '../resources/predict_blur.jpg'
 import hero from '../resources/hero_blur.jpg'
+import roshan from '../resources/roshan_blur.jpg'
+import dev from '../resources/dev.jpg'
 
 class Home extends Component{
 
     render(){
-
+        let  classname = ['red darken-4','pink darken-4','purple darken-4','deep-purple darken-4','indigo darken-4','blue darken-4','light-blue darken-4','cyan darken-4','teal darken-4','green darken-4',' light-green darken-4','lime darken-4','amber darken-4','blue-grey darken-4']
+        classname = classname.map( cls => 'card '+cls)
         return(
             <div>
                 <Navbar />
@@ -22,12 +25,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6">
                                 <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={heros} alt='hero'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6">
-                                <div className=" card blue-grey darken-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
@@ -43,12 +46,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6 push-m6 push-l6">
                             <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={hero} alt='dota 2'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6 pull-m6 pull-l6">
-                                <div className=" card blue-grey lighten-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
@@ -64,12 +67,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6">
                             <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={predict} alt='dota 2'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6">
-                                <div className=" card blue-grey lighten-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
@@ -85,12 +88,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6 push-m6 push-l6">
                             <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={counter} alt='dota 2'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6 pull-m6 pull-l6">
-                                <div className=" card blue-grey lighten-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
@@ -106,12 +109,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6">
                             <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={roshan} alt='dota 2 roshan'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6">
-                                <div className=" card blue-grey lighten-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
@@ -127,12 +130,12 @@ class Home extends Component{
                             <div className="col s12 m6 l6 push-m6 push-l6">
                             <div className="card hoverable">
                                     <div className="card-image">
-                                        <img src={heros} />   
+                                        <img src={dev} alt='contact the developer'/>   
                                     </div>
                                 </div>
                             </div>
                             <div className="container col s12 m6 l6 pull-m6 pull-l6">
-                                <div className=" card blue-grey lighten-3">
+                                <div className={classname[Math.floor(Math.random()*classname.length)]}>
                                     <div className="card-content white-text">
                                         <p>All the Dota 2 Heros information you need to know , In same place</p>
                                     </div>
