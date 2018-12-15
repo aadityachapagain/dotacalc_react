100a88dc8207400ab17f155d0ac00c5f8da7d8cb
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-15 13:42:54 +0545

    resources

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './nav'
 import '../css/home.css'
-import dotablur from '../resources/dota_blur.jpg'
+import dotablur from '../resources/dota_back_blur.jpg'
 
 class Home extends Component{
 
@@ -12,10 +12,53 @@ class Home extends Component{
                 <Navbar />
                 <img src={dotablur} id="back-ground" />
                 <div className="container">
-                    
-                    <p> 
-                        This is home page!
-                    </p>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6 push-m6 push-l6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6 push-m6 push-l6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="section">
+                        <div className="row">
+                            <div className="col s12 m6 l6 push-m6 push-l6">
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
