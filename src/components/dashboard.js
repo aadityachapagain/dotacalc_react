bbc4f8b991c4e58c2a69150890f6813dc191161b
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-01-02 15:22:26 +0545

    heros selection dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,7 +4,20 @@ class DashBoard extends Component{
 
     render(){
         return(
-            <div className="Dashboard"></div>
+            <div className="Dashboard">
+                <div className="row">
+                    <div className="container col l6 m6 s6">
+                        <p>THE RADIANT</p>
+                        <br />
+                        <br />
+                    </div>
+                    <div className="container col l6 m6 s6">
+                        <p>THE DIRE</p>
+                        <br />
+                        <br />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
